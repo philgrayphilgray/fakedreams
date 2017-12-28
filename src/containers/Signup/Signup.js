af27f44ea0d968457c9ee01e1f1494655a8dce68
philgrayphilgray <you@example.comclear>
2017-12-28 15:46:46 -0500

    handle authentication in signup page

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -1,54 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 import FormInput from "../../components/UI/FormInput/FormInput";
 import Button from "../../components/UI/Button/Button";
 import styles from "./Signup.css";
-
+import * as actions from "../../store/actions";
 import Layout from "../../hoc/Layout/Layout";
 
-const Signup = props => {
-  return (
-    <Layout>
-      <div className={styles.Signup}>
-        <div className={styles.SignupForm}>
-          <Button type="facebook">Signup with Facebook</Button>
-          <div>
+class Signup extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  onChangeEmail = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+  onChangePassword = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  attemptLogin = e => {
+    e.preventDefault();
+    this.props.authenticate(this.state.email, this.state.password);
+  };
+  render() {
+    return (
+      <Layout>
+        {this.props.isAuthenticated ? <Redirect to="/profile" /> : null}
+        <div className={styles.Signup}>
+          <div className={styles.SignupForm}>
+            <Button type="facebook">Signup with Facebook</Button>
             <div>
+              <div>
+                <p>
+                  <strong>or sign up with your email address</strong>
+                </p>
+              </div>
+              <form onSubmit={this.attemptLogin}>
+                <fieldset>
+                  <ul>
+                    <FormInput
+                      name="register-email"
+                      type="email"
+                      placeholder="email"
+                      change={this.onChangeEmail}
+                      value={this.state.email}
+                      required
+                    />
+                    <FormInput
+                      name="confirm-email"
+                      type="email"
+                      placeholder="confirm email"
+                    />
+                    <FormInput
+                      name="register-password"
+                      type="password"
+                      placeholder="password"
+                      change={this.onChangePassword}
+                      value={this.state.password}
+                      required
+                    />
+                  </ul>
+                </fieldset>
+                <Button type="success">Signup</Button>
+              </form>
               <p>
-                <strong>or sign up with your email address</strong>
+                Already have an account? <Link to="/login">Login</Link>
               </p>
             </div>
-            <form>
-              <fieldset>
-                <ul>
-                  <FormInput
-                    name="register-email"
-                    type="email"
-                    placeholder="email"
-                  />
-                  <FormInput
-                    name="confirm-email"
-                    type="email"
-                    placeholder="confirm email"
-                  />
-                  <FormInput
-                    name="register-password"
-                    type="password"
-                    placeholder="password"
-                  />
-                </ul>
-              </fieldset>
-              <Button type="success">Signup</Button>
-            </form>
-            <p>
-              Already have an account? <Link to="/login">Login</Link>
-            </p>
           </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  };
 };
 
-export default Signup;
+const mapDispatchToProps = dispatch => {
+  //hardcode isSignup value for now.
+  return {
+    authenticate: (userid, password) =>
+      dispatch(actions.auth(userid, password, true))
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Signup));
