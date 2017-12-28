ceabaefdc4317c8ab735939b2a86130f0f941aae
philgrayphilgray <you@example.comclear>
2017-12-28 15:49:12 -0500

    begin setting up profile page for custom user content

--- src/containers/ProfilePage/ProfilePage.js
+++ src/containers/ProfilePage/ProfilePage.js
@@ -1,19 +1,60 @@
 import React, { Component } from "react";
 import Layout from "../../hoc/Layout/Layout";
+import { connect } from "react-redux";
 
+import * as actions from "../../store/actions";
 import Button from "../../components/UI/Button/Button";
-import RichTextEditor from "../../components/UI/RichTextInput/RichTextInput";
+// import RichTextEditor from "../../components/UI/RichTextInput/RichTextInput";
+import FormInput from "../../components/UI/FormInput/FormInput";
+import styles from "./ProfilePage.css";
 
 class ProfilePage extends Component {
+  state = {
+    userMessage: ""
+  };
+
+  onChangeMessage = e => {
+    this.setState({ userMessage: e.target.value });
+  };
   render() {
     return (
       <Layout>
-        <h2>Profile</h2>
-        <RichTextEditor />
-        <Button type="success">Save</Button>
+        <div className={styles.ProfilePage}>
+          <h2>Profile</h2>
+          <hr />
+          <h3>Username</h3>
+          <p>{this.props.user}</p>
+          <h3>Message</h3>
+          {this.state.userMessage ? <pre>{this.state.userMessage}</pre> : null}
+          <ul>
+            <FormInput
+              name="bio"
+              type="text"
+              placeholder="your message"
+              value={this.state.userMessage}
+              change={this.onChangeMessage}
+            />
+          </ul>
+          <Button type="success">Save</Button>
+          <Button type="danger" clicked={this.props.logout}>
+            Logout
+          </Button>
+        </div>
       </Layout>
     );
   }
 }
 
-export default ProfilePage;
+const mapStateToProps = state => {
+  return {
+    user: state.email
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    logout: () => dispatch(actions.logout())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
