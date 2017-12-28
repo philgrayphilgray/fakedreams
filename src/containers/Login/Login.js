ceb439ab179cd0678e6bea2e83128b29ea333976
philgrayphilgray <you@example.comclear>
2017-12-28 15:47:29 -0500

    handle authentication in login page

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 
+import { connect } from "react-redux";
+
 import FormInput from "../../components/UI/FormInput/FormInput";
 import Button from "../../components/UI/Button/Button";
 import styles from "./Login.css";
-
+import * as actions from "../../store/actions";
 import Layout from "../../hoc/Layout/Layout";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  onChangeEmailHandler = e => {
+    this.setState({ email: e.target.value });
+  };
+  onChangePasswordHandler = e => {
+    this.setState({ password: e.target.value });
+  };
+
   submitLoginHandler = e => {
     e.preventDefault();
-    this.props.history.push("/profile");
+    // this.props.history.push("/profile");
+    this.props.login(this.state.email, this.state.password);
   };
 
   render() {
@@ -22,24 +37,26 @@ class Login extends Component {
               Login with Facebook
             </Button>
             <div>
-              <form>
+              <form onSubmit={this.submitLoginHandler}>
                 <fieldset>
                   <ul>
                     <FormInput
                       name="auth-email"
                       type="email"
                       placeholder="email"
+                      required
+                      change={this.onChangeEmailHandler}
                     />
                     <FormInput
                       name="auth-password"
                       type="password"
                       placeholder="password"
+                      required
+                      change={this.onChangePasswordHandler}
                     />
                   </ul>
                 </fieldset>
-                <Button type="success" clicked={this.submitLoginHandler}>
-                  Login
-                </Button>
+                <Button type="success">Login</Button>
               </form>
               <p>
                 <Link to="/">Forgot your username or password?</Link>
@@ -55,4 +72,10 @@ class Login extends Component {
   }
 }
 
-export default withRouter(Login);
+const mapDispatchToProps = dispatch => {
+  return {
+    login: (email, password) => dispatch(actions.auth(email, password, false))
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(Login));
