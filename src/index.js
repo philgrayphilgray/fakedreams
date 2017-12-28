45a1fd568d9a2fb29d4d2aae7aab037e8de25919
philgrayphilgray <you@example.comclear>
2017-12-28 15:42:34 -0500

    setup axios and thunk for async functions in redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import thunk from "redux-thunk";
 import reducer from "./store/reducer";
 import App from "./App";
 import "./index.css";
 
 import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const app = (
   <Provider store={store}>
